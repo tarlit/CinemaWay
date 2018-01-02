baf5ab8f70551acb53f7be1843ee3511a6503009
tarlit <tarlitan@gmail.com>
2018-01-03 00:07:32 +0200

    InitialCommit

---
+++ CinemaWay.Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CinemaWay.Data.Models;
+
+namespace CinemaWay.Data
+{
+    public class CinemaWayDbContext : IdentityDbContext<User>
+    {
+        public CinemaWayDbContext(DbContextOptions<CinemaWayDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
